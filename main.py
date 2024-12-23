3b45b4adaac2f6596f57d0a181b82fe8dd4f9c03
time4soup <zak.kirk00@gmail.com>
2024-12-22 22:45:00 -0800

    added player class and object

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        window.fill("black")
+        
+        screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")

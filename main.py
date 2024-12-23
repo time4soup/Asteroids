71db74b6c2e6f7d1ac58ed0fd3edbed13166b1d7
time4soup <zak.kirk00@gmail.com>
2024-12-22 22:12:54 -0800

    Set up basic game loop for Pygame

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        window.fill("black")
+        pygame.display.flip()
+    
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
